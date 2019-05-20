72f355d76ff5f36e25258498e6307d35b27d3e1f
Mannan Qazi <mannanqazi2@gmail.com>
2019-05-19 22:17:29 -0400

    Added context api forstate management

--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -2,20 +2,32 @@ import React, { Component } from 'react';
 import Product from './Product';
 import Title from './Title';
 import { storeProducts } from '../data';
+import { ProductConsumer } from '../context';
 
 export default class ProductList extends Component {
     state = {
         product: storeProducts
     };
     render() {
-        console.log(this.state.product);
         return (
             <React.Fragment>
                 <div className="py-5">
                     <div className="container">
                         <Title name="our" title="products" />
                         {/* proucts row */}
-                        <div className="row">{/* prodlist */}</div>
+                        <div className="row">
+                            {/* prodlist */}
+                            <ProductConsumer>
+                                {value => {
+                                    return value.products.map(product => (
+                                        <Product
+                                            product={product}
+                                            key={product.id}
+                                        />
+                                    ));
+                                }}
+                            </ProductConsumer>
+                        </div>
                     </div>
                 </div>
             </React.Fragment>
